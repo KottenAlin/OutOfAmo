ab6f7bb36a24612c6b69a82ae71bdc7aa77c6829
sebastian200 <kotte.alin@gmail.com>
2024-09-03 15:11:06 +0200

    Add collision detection for player and hazard, for taking damage

--- My project/Assets/Scripts/Enemy/HazardScript.cs
+++ My project/Assets/Scripts/Enemy/HazardScript.cs
@@ -7,22 +7,24 @@ public class HazardScript : MonoBehaviour
 {
     
     public PlayerHealth playerHealth;
+    public CapsuleCollider playerCollider;
+    public BoxCollider hazardCollider;
 
-    public BoxCollider collider;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damageInterval = 100f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    private float nextDamageTime = 0f;
 
     void FixedUpdate()
     {
+        if (playerCollider.bounds.Intersects(hazardCollider.bounds)) //if the player collides with the hazard, take damage
+        {
+            if (Time.time >= nextDamageTime) //if the player is not invincible (invincibility time is over)
+            {
+                playerHealth.TakeDamage(damage);
+                nextDamageTime = Time.time + damageInterval;
+            }
+        }
+    }
 
-}
-}
+    }
