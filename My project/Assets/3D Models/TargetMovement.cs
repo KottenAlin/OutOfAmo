fb08f52ab0c8cc478a5f9880e28c9c3f7c87068f
abbvighai <viggo.haimanas.bygden@abbgymnasiet.se>
2024-09-10 10:53:23 +0200

    Killable Palme

--- My project/Assets/3D Models/TargetMovement.cs
+++ My project/Assets/3D Models/TargetMovement.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PalmeMovement : MonoBehaviour
+public class TargetMovement : MonoBehaviour
 {
+    public Vector3 destination;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
