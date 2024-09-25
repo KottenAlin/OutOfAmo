af8f93659039116c68422ec0449e641fb96c7c97
sebastian200 <kotte.alin@gmail.com>
2024-09-25 09:10:20 +0200

    added Tutorial finctionality and moevement arrows

--- My project/Assets/MovementTutorialUI.cs
+++ My project/Assets/MovementTutorialUI.cs
@@ -6,21 +6,27 @@ public class MovementTutorialUI : MonoBehaviour
 {
     public float speed = 4.0f;
     public float amplitude = 1f;
+    float randNum = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+    randNum = Random.Range(0.8f, 1f); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float height = Mathf.Sin(Time.time * speed) * amplitude;
+        
+        float height = Mathf.Sin(Time.time * speed *randNum) * amplitude;
+       
         Transform transform = GetComponent<Transform>();
         if (transform != null)
         {
-            transform.position = new Vector3(transform.position.x, height, transform.position.z);
-    
+            Vector3 newPosition = transform.position;
+            newPosition.y = newPosition.y + height/40f;
+            transform.position = newPosition;
+
         }
     }
 }
